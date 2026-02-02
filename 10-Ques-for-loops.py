29f1fd12b1a7f86f10201635ad0c409cbcd770a9
Harsh jain <harshjn782@gmail.com>
2026-02-02 12:06:45 +0530

    Implement multiplication table using for loop
    Add a program to print multiplication table using a for loop.

---
+++ 10-Ques-for-loops.py
@@ -0,0 +1,7 @@
+Write a Python program to print each character of a string one by one using a for loop.
+
+Like this
+n = int(input("Enter a number: "))
+
+for i in range(1, 11):
+print(f"{n} × {i} = {n * i}")
