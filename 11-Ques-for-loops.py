a1ba54ddab1860da0f359d8f8210811c4b26c7f0
Harsh jain <harshjn782@gmail.com>
2026-02-04 17:10:56 +0530

    Add program to sum numbers from 1 to 100
    Implement a program to calculate the sum of numbers from 1 to 100 using a for loop.

---
+++ 11-Ques-for-loops.py
@@ -0,0 +1,8 @@
+ # Write a Python program to find the sum of numbers from 1 to 100 using a for loop.
+
+t = 0
+
+for i in range(1, 101):
+t += i
+
+print(t)                                                                      
